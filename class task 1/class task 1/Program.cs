8db9332c8c55557d1dd272aa542fa3d0c1ed7b61
Nasserxald <107666420+Nasserxald@users.noreply.github.com>
2022-06-22 19:45:19 +0300

    first project

---
+++ class task 1/class task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string HeroName = "leon";
+            double HeroHeight = 187.5 + 5;
+            int HeroAge = 27;
+            string HeroSuperPower = "super double kick";
+            string VillainName = "Nemesis";
+            double villainHeight = 210.2;
+            int villainAge = 32;
+            string villainSuperPower = "None";
+            int AgeDifference = HeroAge - villainAge;
+            Console.WriteLine(" my Name" + HeroName + " MY age is " + HeroAge + " My Super powers is " + HeroSuperPower + " My height is " + HeroHeight);
+            Console.WriteLine(" I am the villain and My Name is " + VillainName + " My age is " + villainAge + " My height is " + villainHeight + " my super powers is " + villainSuperPower);
+        }
+    }
+}
